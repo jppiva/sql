4ef2995a56dfa52ca63e1bdeb05d1f19f9545248
joao pedro piva <joaopedropiva17@gmail.com>
2025-09-22 09:17:25 -0300

    criação delete

---
+++ 03-update.sql
@@ -0,0 +1,13 @@
+UPDATE usuario SET username = 'ProGamer2025'
+WHERE username = 'NoobMaster';
+
+SELECT * FROM usuario;
+
+UPDATE usuario SET pontos = pontos + 1000;
+
+SELECT * FROM usuario
+
+UPDATE usuario SET email = 'main@gmail.com';
+WHERE email = 'master@email.com';
+
+SELECT * FROM usuario;
---
+++ 04-delete.sql
@@ -0,0 +1,16 @@
+DELETE FROM usuario
+WHERE username = 'CheatCodeKing';
+
+SELECT * FROM usuario;
+
+DELETE FROM usuario
+WHERE id = 13;
+
+SELECT * FROM usuario;
+
+DELETE FROM usuario;
+
+DELETE FROM usuario
+WHERE pontos < 2000;
+
+SELECT * FROM usuario;
